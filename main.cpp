7dc741cde185df0a98fa74020b95f9883db16b5e
nirmal <mcnirmalkumar00@gmail.com>
2025-11-13 23:14:32 +0530

    added cancel pomo option

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ class pomo {
     int get_current_time_in_mins();
     void draw_main_tui(int choice);
     int draw_menu_driven_tui();
+    void cancel_pomo();
 
     int choice;
 };
@@ -66,6 +67,9 @@ void pomo::draw_main_tui(int choice) {
 
   count_down(sesh_len, heading_win);
 
+  mvwprintw(heading_win, 6,1, "Pomo Session has ended.");
+  wrefresh(heading_win);
+
   getch();
   endwin();
 
@@ -85,7 +89,7 @@ int pomo::draw_menu_driven_tui() {
   box(menu_win, 0, 0);
 
   mvwprintw(menu_win, 1,1, "Welcome to Nirmal's PomoDoro!");
-  mvwprintw(menu_win, 2,1, "Press 1 for default 25 minutes Pomo. \t\t\t Press 2 for custom Pomo timer.");
+  mvwprintw(menu_win, 2,1, "Press 1 for default 25 minutes Pomo. \t\t\t Press 2 for custom Pomo timer. ");
   wrefresh(menu_win);
 
   echo();
@@ -104,6 +108,10 @@ int pomo::draw_menu_driven_tui() {
 
 }
 
+void pomo::cancel_pomo() {
+  
+}
+
 void pomo::count_down(int sesh_len_min, WINDOW *heading_win) {
   
   int current_min = get_current_time_in_mins();
@@ -112,6 +120,10 @@ void pomo::count_down(int sesh_len_min, WINDOW *heading_win) {
 
   mvwprintw(heading_win, 3, 1, "Pomo Session for %d minutes has started.", sesh_len_min);
   wrefresh(heading_win);
+  
+  mvwprintw(heading_win, 5, 1, "Press q to cancel pomo : ");
+  
+  nodelay(heading_win, TRUE); //makes input non blocking for other functions of heading_win
 
   auto start = chrono::steady_clock::now(); //gets current time as a chrono object 
   auto duration = chrono::minutes(sesh_len_min); //converts user mins input into a chrono object 
@@ -123,11 +135,17 @@ void pomo::count_down(int sesh_len_min, WINDOW *heading_win) {
     mvwprintw(heading_win, 4, 1, "Time left : ");
     mvwprintw(heading_win, 4, 14, "%d", sesh_len_min - (seconds/60));
     wrefresh(heading_win);
+    
+    int ch = wgetch(heading_win); //wgetnstr is blocking, hence not used
+
+    if (ch == 'q' || ch == 'Q') {
+      mvwprintw(heading_win, 6,1, "Pomo Doro Cancelled");
+      wrefresh(heading_win);
+      return; //exit loop early:
+    }
+    
     this_thread::sleep_for(chrono::seconds(1));
   }
-
-  mvwprintw(heading_win, 5, 1, "Pomo Session has ended.");
-  wrefresh(heading_win);
 
 }
 
@@ -145,9 +163,11 @@ int pomo::get_current_time_in_mins() {
 int main() {
 
   pomo p;
-  
-  p.choice = p.draw_menu_driven_tui();
-  p.draw_main_tui(p.choice);
+
+  while (true) {  
+    p.choice = p.draw_menu_driven_tui();
+    p.draw_main_tui(p.choice);
+  }
 
 
   return 0;
